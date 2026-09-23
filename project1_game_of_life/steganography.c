9742ec59b240027e9e71111fe8072d82c798b735
syk1231233 <1846954776@qq.com>
2026-09-23 14:36:54 +0800

    proj1 done parta2

--- project1_game_of_life/steganography.c
+++ project1_game_of_life/steganography.c
@@ -20,15 +20,67 @@
 
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
-{
-	//YOUR CODE HERE
+{	
+	// 异常值检查
+	if(image == NULL || image->image == NULL || row >= image->rows || col >= image->cols || image->image[row] == NULL) return NULL;
+
+	Color* pixel = (Color*)malloc(sizeof(Color));
+	if(pixel == NULL) return NULL;
+
+	if(image->image[row][col].B & 0x1){ // 最低位为1， 全部改为白色
+		pixel->R = 255;
+		pixel->G = 255;
+		pixel->B = 255;
+	}else{
+		pixel->R = 0;
+		pixel->G = 0;
+		pixel->B = 0;
+	}
+	return pixel;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
-{
-	//YOUR CODE HERE
-}
+{	
+	if(image == NULL || image->image == NULL) return NULL;
+
+	Image* new_image = (Image*)malloc(sizeof(struct Image));
+	if(new_image == NULL) return NULL;
+
+	new_image->cols = image->cols;
+	new_image->rows = image->rows;
+
+	new_image->image = (Color**)malloc(new_image->rows * sizeof(struct Color*));
+	if(new_image->image == NULL){
+		free(new_image);
+		return NULL;
+	}for(int i = 0; i < new_image->rows; i++){
+		new_image->image[i] = (Color*)malloc(new_image->cols * sizeof(Color));
+		if(new_image->image[i] == NULL){
+			while(i > 0){
+				i--;
+				free(new_image->image[i]);
+			}
+			free(new_image->image);
+			free(new_image);
+			return NULL;
+		}
+	}
+
+	for(int y = 0; y < new_image->rows; y++){
+		for(int x = 0; x < new_image->cols; x++){
+			Color* pixel = evaluateOnePixel(image,y,x);
+			if(pixel == NULL){
+				freeImage(new_image);
+				return NULL;
+			}
+			new_image->image[y][x] = *pixel;
+			free(pixel);
+		}
+	}
+
+	return new_image;
+}	
 
 /*
 Loads a file of ppm P3 format from a file, and prints to stdout (e.g. with printf) a new image, 
@@ -44,6 +96,23 @@ Otherwise, you should return from main with code 0.
 Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
-{
-	//YOUR CODE HERE
+{	
+	if(argc != 2 || argv[1] == NULL) return -1;
+
+	// 读取ppm文件
+	Image* image = readData(argv[1]);
+	if(image == NULL) return -1;
+
+	// 解码ppm文件
+	Image* convert_image = steganography(image);
+	if(convert_image == NULL){
+		freeImage(image);
+		return -1;
+	};
+	freeImage(image);
+
+	writeData(convert_image);
+	freeImage(convert_image);
+
+	return 0;
 }
